2e6178ab7ba081254811d755266b3a79774cd427
Lola <lmariemeehan@gmail.com>
2020-01-16 21:46:17 -0800

    Started INR recording app

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Header from '../components/Header';
+
+const Index = () => (
+  <div>
+    <Header />
+    <h1>Hello INR Recorders!</h1>
+  </div>
+)
+
+export default Index;
---
+++ pages/profile.js
@@ -0,0 +1,10 @@
+import Header from '../components/Header';
+
+export default function Profile() {
+  return (
+    <div>
+      <Header />
+      <h2>This is your profile page</h2>
+    </div>
+  );
+}
